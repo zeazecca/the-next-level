4a2a53713c7b6fba15ccebb0ca2bbd2b4305548a
zeazecca <zeazecca@gmail.com>
2020-10-09 17:16:50 +0200

    add dead code elimination to build

--- calc.js
+++ calc.js
@@ -6,25 +6,6 @@ function $extend(from, fields) {
 	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
 	return proto;
 }
-var IntAdapter = function(input) {
-	this.input = input;
-};
-IntAdapter.__name__ = true;
-IntAdapter.prototype = {
-	get: function() {
-		if(this.input.value.length == 0) {
-			return null;
-		}
-		var value = Std.parseInt(this.input.value);
-		if(value != null) {
-			return value;
-		}
-		throw new js__$Boot_HaxeError("could not parse " + Std.string(this.input) + " as int");
-	}
-	,set: function(val) {
-		this.input.placeholder = val == null ? "null" : "" + val;
-	}
-};
 var FloatAdapter = function(input) {
 	this.input = input;
 };
@@ -42,27 +23,6 @@ FloatAdapter.prototype = {
 	}
 	,set: function(val) {
 		this.input.placeholder = FloatExtensions.toString(val,1);
-	}
-};
-var RangeAdapter = function(input) {
-	this.input = input;
-};
-RangeAdapter.__name__ = true;
-RangeAdapter.prototype = {
-	get: function() {
-		if(this.input.value.length == 0) {
-			return null;
-		}
-		var value = parseFloat(this.input.value);
-		if(value != null) {
-			return value;
-		}
-		throw new js__$Boot_HaxeError("could not parse " + Std.string(this.input) + " as float");
-	}
-	,set: function(val) {
-		var tmp = "" + FloatExtensions.toString(val.min,1) + "—";
-		var tmp1 = FloatExtensions.toString(val.max,1);
-		this.input.placeholder = tmp + tmp1;
 	}
 };
 var CalcResults = function(lvl,hp,ac,dmg,hit) {
@@ -280,9 +240,6 @@ FloatExtensions.__name__ = true;
 FloatExtensions.clamp = function(val,min,max) {
 	return Math.max(min,Math.min(max,val));
 };
-FloatExtensions.toRange = function(val) {
-	return new Range(val,val);
-};
 FloatExtensions.round = function(val,precision) {
 	val *= Math.pow(10.0,precision);
 	return Math.round(val) / Math.pow(10.0,precision);
@@ -413,48 +370,10 @@ Main.main = function() {
 	});
 };
 Math.__name__ = true;
-var Range = function(min,max) {
-	if(min < max) {
-		this.min = min;
-		this.max = max;
-	} else {
-		this.max = min;
-		this.min = max;
-	}
-	this.average = (this.max + this.min) / 2.0;
-};
-Range.__name__ = true;
-Range.prototype = {
-	times: function(factor) {
-		return new Range(this.min * factor,this.max * factor);
-	}
-	,div: function(quotient) {
-		return new Range(this.min / quotient,this.max / quotient);
-	}
-};
 var Std = function() { };
 Std.__name__ = true;
 Std.string = function(s) {
 	return js_Boot.__string_rec(s,"");
-};
-Std.parseInt = function(x) {
-	if(x != null) {
-		var _g = 0;
-		var _g1 = x.length;
-		while(_g < _g1) {
-			var i = _g++;
-			var c = x.charCodeAt(i);
-			if(c <= 8 || c >= 14 && c != 32 && c != 45) {
-				var v = parseInt(x, (x[(i + 1)]=="x" || x[(i + 1)]=="X") ? 16 : 10);
-				if(isNaN(v)) {
-					return null;
-				} else {
-					return v;
-				}
-			}
-		}
-	}
-	return null;
 };
 var js__$Boot_HaxeError = function(val) {
 	Error.call(this);
@@ -541,11 +460,6 @@ Object.defineProperty(js__$Boot_HaxeError.prototype,"message",{ get : function()
 	return String(this.val);
 }});
 js_Boot.__toStr = ({ }).toString;
-Data.PLAYER_HITPROB = 0.6;
-Data.CREATURE_HITPROB = 0.6;
-Data.MIN_HIT = 0.05;
-Data.MAX_HIT = 0.95;
-Data.PROB_INCREMENTS = 0.05;
 Data.STATS = [new XpEntry(-3,14.81336806,14,1.807830801,5),new XpEntry(-2,16.22723167,14,1.98037942,5),new XpEntry(-1,17.77604167,14,2.169396961,5),new XpEntry(0,19.47267801,14,2.376455304,5),new XpEntry(1,21.33125,14,2.603276353,5),new XpEntry(2,22.53402778,14,4.515669516,5),new XpEntry(3,30.04097222,14,6.428062678,5),new XpEntry(4,32.39097222,15,8.34045584,6),new XpEntry(5,53.70069444,16,10.252849,7),new XpEntry(6,54.80069444,16,12.16524217,7),new XpEntry(7,56.50694444,16,14.07763533,7),new XpEntry(8,59.20694444,17,16.16096866,8),new XpEntry(9,62.99444444,18,18.09472934,9),new XpEntry(10,62.99444444,18,20.24216524,9),new XpEntry(11,79.43541667,18,22.19729345,9),new XpEntry(12,79.43541667,18,27.48575499,9),new XpEntry(13,81.87291667,19,29.71866097,10),new XpEntry(14,89.12916667,19,31.95156695,10),new XpEntry(15,91.98125,19,34.18447293,10),new XpEntry(16,93.78125,19,40.51994302,10),new XpEntry(17,127.835,20,43.00925926,11),new XpEntry(18,127.835,20,45.4985755,11),new XpEntry(19,130.11,20,52.0477208,11),new XpEntry(20,150.9808333,20,55.60541311,11),new XpEntry(21,168.8017033,20,62.16874181,11),new XpEntry(22,188.7260417,20,69.50676638,11),new XpEntry(23,211.0021291,20,77.71092726,11),new XpEntry(24,235.9075521,20,86.88345798,11),new XpEntry(25,263.7526614,20,97.13865908,11),new XpEntry(26,294.8844401,20,108.6043225,11),new XpEntry(27,329.6908268,20,121.4233238,11),new XpEntry(28,368.6055501,20,135.7554031,11),new XpEntry(29,412.1135335,20,151.7791548,11),new XpEntry(30,460.7569377,20,169.6942539,11)];
 Data.MIN_LVL = Lambda.fold(Data.STATS.slice(1),function(entry,min) {
 	if(entry.lvl < min.lvl) {
